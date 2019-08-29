c733a6ec3abc1a215a5cbab1e93f9fc9430214d2
OyamaPlati <platioyama@gmail.com>
2019-08-29 14:37:08 +0200

    added sequential commit comment

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,7 @@ public class Main {
         // Take the following command-line arguments <data file name> <output file name>
         String dataFile = args[0];
         String outputFile = args[1];
-
+        // Compute sequentially
         CloudData cloudData = new CloudData();
         cloudData.readData(dataFile);
         cloudData.writeData(outputFile);
